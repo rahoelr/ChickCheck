f13a0b5ee92920260c43a234a1dbd9ee88e6e64d
rahul rahmatullah <rahul.rahmatullah@mail.ugm.ac.id>
2025-01-16 16:22:09 +0700

    feat : add login, register, and main act

---
+++ app/src/main/java/com/example/chickcheck/login/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.chickcheck.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.chickcheck.R
+import com.example.chickcheck.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
